9ae3fa930dffd2b67ff7c4a3a6932485019b91aa
khajornkiat usap <khajornkiat.us@ols.co.th>
2022-06-07 21:55:39 +0700

    test things

--- point-of-sale/src/client/components/pages/App.tsx
+++ point-of-sale/src/client/components/pages/App.tsx
@@ -16,6 +16,8 @@ import { TransactionsProvider } from '../contexts/TransactionsProvider';
 import { SolanaPayLogo } from '../images/SolanaPayLogo';
 import { SOLIcon } from '../images/SOLIcon';
 import css from './App.module.css';
+import { MAINNET_ENDPOINT, MAINNET_USDC_MINT } from '../../utils/constants';
+import { USDCIcon } from '../images/USDCIcon';
 
 interface AppProps extends NextAppProps {
     host: string;
